0de075fa642f3fb98d0592ce84b5362c251be3d9
JorgeRamirezDEV <jorgeramirezinstituto@gmail.com>
2021-02-09 11:14:24 +0100

    .

--- logout.php
+++ logout.php
@@ -1,1 +1,9 @@
 <?php
+session_start();
+// Eliminar todas las variables de sesión
+session_unset();
+// Eliminar la sesión
+session_destroy();
+
+// Redirigir a Login
+header("Location: login.php");
